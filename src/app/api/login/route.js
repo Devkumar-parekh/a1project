43ea87fa357609d61d3d3ee783d9e43f8c4fe0fd
Devkumar Parekh <67862268+Devkumar-parekh@users.noreply.github.com>
2024-09-23 23:22:02 +0530

    order placed, canceled, log out, ltp

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -21,12 +21,86 @@ export const POST = async (req, res) => {
       payload.totp
     );
     let profile = {};
+    let orderBook = {};
+    let tradeBook = {};
+    let holding = {};
+    let placeOrderStatus = {};
+    let ltpData = {};
     if (res) {
       profile = await smart_api.getProfile();
+
+      // User Methods
+      // return smart_api.getProfile()
+
+      // return smart_api.logout()
+
+      // return smart_api.getRMS();
+
+      // Order Methods
+      // placeOrderStatus = smart_api.placeOrder({
+      //   variety: "NORMAL",
+      //   tradingsymbol: "SBIN-EQ",
+      //   symboltoken: "3045",
+      //   transactiontype: "BUY",
+      //   exchange: "NSE",
+      //   ordertype: "LIMIT",
+      //   producttype: "INTRADAY",
+      //   duration: "DAY",
+      //   price: "1",
+      //   squareoff: "0",
+      //   stoploss: "0",
+      //   quantity: "1",
+      // });
+      // console.log(placeOrderStatus.data, "placeOrderStatus");
+
+      // return smart_api.modifyOrder({
+      //     "orderid": "201130000006424",
+      //     "variety": "NORMAL",
+      //     "tradingsymbol": "SBIN-EQ",
+      //     "symboltoken": "3045",
+      //     "transactiontype": "BUY",
+      //     "exchange": "NSE",
+      //     "ordertype": "LIMIT",
+      //     "producttype": "INTRADAY",
+      //     "duration": "DAY",
+      //     "price": "19500",
+      //     "squareoff": "0",
+      //     "stoploss": "0",
+      //     "quantity": "1"
+      // });
+
+      // return smart_api.cancelOrder({
+      //     "variety": "NORMAL",
+      //     "orderid": "201130000006424"
+      // });
+
+      orderBook = await smart_api.getOrderBook();
+
+      tradeBook = await smart_api.getTradeBook();
+
+      // Portfolio Methods
+      // holding = smart_api.getHolding();
+
+      // return smart_api.getPosition();
+
+      // return smart_api.convertPosition({
+      //     "exchange": "NSE",
+      //     "oldproducttype": "DELIVERY",
+      //     "newproducttype": "MARGIN",
+      //     "tradingsymbol": "SBIN-EQ",
+      //     "transactiontype": "BUY",
+      //     "quantity": 1,
+      //     "type": "DAY"
+      // });
     }
     return NextResponse.json({
       res,
       profile,
+      orderBook,
+      tradeBook,
+      holding,
+      placeOrderStatus,
+      ltpData,
     });
   } catch (error) {
     return NextResponse.json({
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,116 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useState, useEffect, memo } from "react";
 import axios from "axios";
 import { TOTP } from "totp-generator";
+import { instruments } from "./assets/instruments";
+
+const Instrument = (props) => {
+  const { name, symbol, exch_seg, index } = props?.data;
+  const { api_key, token } = props;
+  const [ltp, setltp] = useState({});
+  console.log(props?.data, "props?.data");
+  useEffect(() => {
+    setltp([]);
+  }, [index]);
+  return (
+    <div
+      key={index}
+      style={{
+        padding: "10px",
+        border: "2px solid gray",
+        borderRadius: "10px",
+        margin: "10px 0",
+      }}
+    >
+      <div className="" style={{ display: "flex", gap: "10px" }}>
+        <div>
+          <div>name: {name}</div>
+          <div>symbol: {symbol}</div>
+          <div>exch_seg: {exch_seg}</div>
+        </div>
+        <div>
+          {props?.data?.token === ltp?.symboltoken && ltp?.symboltoken && (
+            <>
+              <div>ltp: {ltp?.ltp}</div>
+              <div>high: {ltp?.high}</div>
+              <div>low: {ltp?.low}</div>
+              <div>open: {ltp?.open}</div>
+              <div>close: {ltp?.close}</div>
+            </>
+          )}
+        </div>
+        <div>
+          {/* {props?.data?.token === ltp?.symboltoken && ltp?.symboltoken && ( */}
+          <button
+            onClick={async () => {
+              const templtp = await getLTP({
+                api_key: api_key,
+                jwt: token?.jwtToken,
+                data: {
+                  exchange: props?.data?.exch_seg,
+                  tradingsymbol: props?.data?.symbol,
+                  symboltoken: props?.data?.token,
+                },
+              });
+              console.log(templtp?.data?.data, "templtpa");
+              setltp(templtp?.data?.data);
+            }}
+          >
+            LTP
+          </button>
+          {/* )} */}
+          {props?.data?.token === ltp?.symboltoken && ltp?.symboltoken && (
+            <button
+              onClick={async () => {
+                const temp = await placeorder({
+                  api_key: api_key,
+                  jwt: token?.jwtToken,
+                  data: {
+                    variety: "NORMAL",
+                    tradingsymbol: "SBIN-EQ",
+                    symboltoken: "3045",
+                    transactiontype: "BUY",
+                    exchange: "NSE",
+                    ordertype: "MARKET",
+                    producttype: "INTRADAY",
+                    duration: "DAY",
+                    price: "1",
+                    squareoff: "0",
+                    stoploss: "0",
+                    quantity: "1",
+                  },
+                });
+                console.log(temp, "♦♥😍temp placeorder");
+              }}
+            >
+              Buy
+            </button>
+          )}
+
+          {props?.data?.token === ltp?.symboltoken && ltp?.symboltoken && (
+            <button
+              onClick={async () => {
+                const temp = await cancelOrder({
+                  api_key: api_key,
+                  jwt: token?.jwtToken,
+                  data: {
+                    variety: "NORMAL",
+                    orderid: "240923001916184",
+                  },
+                });
+                console.log(temp, "♦♥😍temp cancelOrder");
+              }}
+            >
+              Cancel Order
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const InputText = (props) => {
   return (
@@ -17,6 +124,7 @@ const InputText = (props) => {
         margin: "3px",
         padding: "5px",
         fontSize: "16px",
+        width: "min(300px,100%)",
       }}
       placeholder={props.placeholder}
     />
@@ -29,7 +137,115 @@ const findmyip = async () => {
   return res.data?.ip;
 };
 
+const cancelOrder = async (payload) => {
+  // var data = JSON.stringify({
+  //   variety: "NORMAL",
+  //   orderid: "201020000000080",
+  // });
+
+  let data = JSON.stringify(payload?.data);
+
+  var config = {
+    method: "post",
+    url: "https://apiconnect.angelone.in/rest/secure/angelbroking/order/v1/cancelOrder",
+    headers: {
+      Authorization: `Bearer ${payload?.jwt}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-UserType": "USER",
+      "X-SourceID": "WEB",
+      "X-ClientLocalIP": "CLIENT_LOCAL_IP",
+      "X-ClientPublicIP": "CLIENT_PUBLIC_IP",
+      "X-MACAddress": "MAC_ADDRESS",
+      "X-PrivateKey": payload.api_key,
+    },
+    data: data,
+  };
+
+  const result = await axios(config);
+  console.log(result);
+  return result;
+};
+
+const placeorder = async (payload) => {
+  let data = JSON.stringify(payload?.data);
+
+  var config = {
+    method: "post",
+    url: "https://apiconnect.angelone.in/rest/secure/angelbroking/order/v1/placeOrder",
+    headers: {
+      Authorization: `Bearer ${payload?.jwt}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-UserType": "USER",
+      "X-SourceID": "WEB",
+      "X-ClientLocalIP": "CLIENT_LOCAL_IP",
+      "X-ClientPublicIP": "CLIENT_PUBLIC_IP",
+      "X-MACAddress": "MAC_ADDRESS",
+      "X-PrivateKey": payload.api_key,
+    },
+    data: data,
+  };
+
+  const result = await axios(config);
+  console.log(result);
+  return result;
+};
+
+const getLTP = async (payload) => {
+  let data = JSON.stringify(payload?.data);
+
+  var config = {
+    method: "post",
+    url: "https://apiconnect.angelone.in/order-service/rest/secure/angelbroking/order/v1/getLtpData",
+    headers: {
+      Authorization: `Bearer ${payload?.jwt}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-UserType": "USER",
+      "X-SourceID": "WEB",
+      "X-ClientLocalIP": "CLIENT_LOCAL_IP",
+      "X-ClientPublicIP": "CLIENT_PUBLIC_IP",
+      "X-MACAddress": "MAC_ADDRESS",
+      "X-PrivateKey": payload.api_key,
+    },
+    data: data,
+  };
+
+  const result = await axios(config);
+  console.log(result);
+  return result;
+};
+const logOut = async (payload) => {
+  var data = JSON.stringify({
+    clientcode: payload?.client_code,
+  });
+  console.log(payload.api_key, "payload.api_key");
+  var config = {
+    method: "post",
+    url: "https://apiconnect.angelone.in/rest/secure/angelbroking/user/v1/logout",
+    headers: {
+      Authorization: `Bearer ${payload?.jwt}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-UserType": "USER",
+      "X-SourceID": "WEB",
+      "X-ClientLocalIP": "CLIENT_LOCAL_IP",
+      "X-ClientPublicIP": "CLIENT_PUBLIC_IP",
+      "X-MACAddress": "MAC_ADDRESS",
+      "X-PrivateKey": payload.api_key,
+    },
+    data: data,
+  };
+
+  const result = await axios(config);
+  console.log(result);
+  alert(result?.data?.data);
+  return result;
+};
+
 export default function Home() {
+  const [data, setData] = useState([]);
   const [formdata, setFormdata] = useState({
     api_key: "",
     client_code: "",
@@ -37,7 +253,7 @@ export default function Home() {
     totp: "",
     type: "",
   });
-  const [response, setResponse] = useState("");
+  const [token, setToken] = useState({});
   const handleFormdata = (e) => {
     let key = e.target.name;
     let value = e.target.value;
@@ -47,9 +263,10 @@ export default function Home() {
   };
   return (
     <>
-      <div className={styles.page}>
-        <main className={styles.main}>
-          {/* <Image
+      <div style={{ display: "flex" }}>
+        <div className={styles.page} style={{ flex: 1 }}>
+          <main className={styles.main} style={{ maxWidth: "400px" }}>
+            {/* <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
@@ -57,103 +274,152 @@ export default function Home() {
           height={38}
           priority
         /> */}
-          <h1 style={{ textAlign: "center" }}>
-            <code>A1PROJECT</code>
-          </h1>
-          {/* <ul>
+            <h1 style={{ textAlign: "center" }}>
+              <code>A1PROJECT</code>
+            </h1>
+            {/* <ul>
           <li>Everything on fingertip</li>
         </ul> */}
-          <hr />
-          <InputText
-            placeholder={"api_key"}
-            name={"api_key"}
-            value={formdata?.api_key}
-            onChange={handleFormdata}
-          />
-          <InputText
-            placeholder={"client_code"}
-            name={"client_code"}
-            value={formdata?.client_code}
-            onChange={handleFormdata}
-          />
-          <InputText
-            type={"password"}
-            placeholder={"password"}
-            name={"password"}
-            value={formdata?.password}
-            onChange={handleFormdata}
-          />
-          <InputText
-            type={"password"}
-            placeholder={"totp"}
-            name={"totp"}
-            value={formdata?.totp}
-            onChange={handleFormdata}
-          />
-          {/* <InputText
-          type={"number"}
-          placeholder={"type"}
-          name={"type"}
-          value={formdata?.type}
-          onChange={handleFormdata}
-        /> */}
-          <hr />
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href=""
-              rel="noopener noreferrer"
-              onClick={async (e) => {
-                e.preventDefault();
-                setResponse("");
-                let mypublicip = await findmyip();
-                const { otp, expires } = TOTP.generate(formdata.totp);
-                const res = await axios.post(`/api/login`, {
-                  ...formdata,
-                  totp: otp,
-                  mypublicip,
-                });
-                if (res) setResponse(JSON.stringify(res?.data));
-              }}
-            >
-              <Image
-                className={styles.logo}
-                src="https://nextjs.org/icons/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
+            <hr />
+            <center>
+              <InputText
+                placeholder={"api_key"}
+                name={"api_key"}
+                value={formdata?.api_key}
+                onChange={handleFormdata}
               />
-              Click To Sign In
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Home
-            </a>
-          </div>
-        </main>
-      </div>
-      {response?.length > 0 && (
-        <div
-          onClick={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <span>
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-          </span>
-          <span>{response || ""}</span>
+              <InputText
+                placeholder={"client_code"}
+                name={"client_code"}
+                value={formdata?.client_code}
+                onChange={handleFormdata}
+              />
+              <InputText
+                type={"password"}
+                placeholder={"password"}
+                name={"password"}
+                value={formdata?.password}
+                onChange={handleFormdata}
+              />
+              <InputText
+                type={"password"}
+                placeholder={"totp"}
+                name={"totp"}
+                value={formdata?.totp}
+                onChange={handleFormdata}
+              />
+            </center>
+            <hr />
+            <div className={styles.ctas}>
+              <button
+                className={styles.primary}
+                href=""
+                rel="noopener noreferrer"
+                onClick={async (e) => {
+                  e.preventDefault();
+
+                  let mypublicip = await findmyip();
+                  const { otp, expires } = TOTP.generate(formdata.totp);
+                  const res = await axios.post(`/api/login`, {
+                    ...formdata,
+                    totp: otp,
+                    mypublicip,
+                  });
+                  setToken(res?.data?.res?.data);
+                }}
+              >
+                <Image
+                  className={styles.logo}
+                  src="https://nextjs.org/icons/vercel.svg"
+                  alt="Vercel logomark"
+                  width={20}
+                  height={20}
+                />
+                Click To Continue
+              </button>
+              <button
+                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.secondary}
+                onClick={async () => {
+                  console.log(formdata?.api_key);
+                  await logOut({
+                    api_key: formdata?.api_key,
+                    jwt: token?.jwtToken,
+                    client_code: formdata?.client_code,
+                  });
+                  setFormdata({});
+                }}
+              >
+                Log out
+              </button>
+            </div>
+            <hr></hr>
+
+            {/* {response?.length > 0 && (
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <span>
+                  <Image
+                    aria-hidden
+                    src="https://nextjs.org/icons/file.svg"
+                    alt="File icon"
+                    width={16}
+                    height={16}
+                  />
+                </span>
+                <span>{response || ""}</span>
+              </div>
+            )} */}
+          </main>
         </div>
-      )}
+        <div style={{ flex: 1, padding: "20px" }}>
+          <InputText
+            placeholder={"Instrument..."}
+            name={"instrument"}
+            value={formdata?.instrument}
+            onChange={handleFormdata}
+          />
+          <button
+            className={styles.primary}
+            style={{ padding: "5px", borderRadius: "10px" }}
+            href=""
+            rel="noopener noreferrer"
+            onClick={async (e) => {
+              setData([]);
+              e.preventDefault();
+              console.log("Searching", formdata?.instrument);
+              let temp = instruments?.filter((item) =>
+                item?.name
+                  ?.toLowerCase()
+                  ?.includes(formdata?.instrument.toLowerCase())
+              );
+              // console.log("test");
+              setData(temp);
+            }}
+          >
+            Search
+          </button>
+          <div>
+            {data?.map((dataitem, index) => {
+              return (
+                <div key={index}>
+                  <Instrument
+                    data={dataitem}
+                    key={index}
+                    api_key={formdata?.api_key}
+                    token={token}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
